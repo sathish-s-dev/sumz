1f0ecb22586a5f08ef80701249b43c22432f11c1
Sathish S <s.sathishmechddesign@gmail.com>
2023-09-07 19:24:27 +0530

    workflow added

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,3 @@
-// import react from '@vitejs/plugin-react';
-// import { defineConfig } from 'vite';
-// // import tsconfigPaths from 'vite-tsconfig-paths';
-
-// // https://vitejs.dev/config/
-
-// export default defineConfig(() => {
-// 	const config = {
-// 		esbuild: {
-// 			loader: 'tsx',
-// 		},
-// 		root: './',
-// 		build: {
-// 			outDir: './dist',
-// 		},
-// 		optimizeDeps: {
-// 			esbuildOptions: {
-// 				loader: {
-// 					'.js': 'jsx',
-// 					'.ts': 'tsx',
-// 				},
-// 			},
-// 		},
-// 		plugins: [react()],
-// 		base: '/sumz',
-// 	}
-
-// 	return config;
-// });
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
