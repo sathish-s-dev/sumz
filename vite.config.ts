ffee56bac66793c7d72c24d1ba019d00272b30cf
Sathish S <s.sathishmechddesign@gmail.com>
2023-09-07 17:25:32 +0530

    config changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,42 @@
-import react from '@vitejs/plugin-react';
+// import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// // import tsconfigPaths from 'vite-tsconfig-paths';
+
+// // https://vitejs.dev/config/
+
+// export default defineConfig(() => {
+// 	const config = {
+// 		esbuild: {
+// 			loader: 'tsx',
+// 		},
+// 		root: './',
+// 		build: {
+// 			outDir: './dist',
+// 		},
+// 		optimizeDeps: {
+// 			esbuildOptions: {
+// 				loader: {
+// 					'.js': 'jsx',
+// 					'.ts': 'tsx',
+// 				},
+// 			},
+// 		},
+// 		plugins: [react()],
+// 		base: '/sumz',
+// 	}
+
+// 	return config;
+// });
+
 import { defineConfig } from 'vite';
-// import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	esbuild: {
-		loader: 'tsx',
-	},
-	root: './',
-	build: {
-		outDir: './dist',
-	},
-	optimizeDeps: {
-		esbuildOptions: {
-			loader: {
-				'.js': 'jsx',
-				'.ts': 'tsx',
-			},
-		},
-	},
-	plugins: [react()],
+export default defineConfig(() => {
+	const config = {
+		plugins: [react()],
+		base: '/sumz',
+	};
+
+	return config;
 });
