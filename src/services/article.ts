06aeea8c1a77a0e29a73d5a4be8600f7408c851d
sathishs007s <110343722+sathishs007s@users.noreply.github.com>
2023-09-07 17:51:36 +0530

    Update article.ts

--- src/services/article.ts
+++ src/services/article.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+const rapidApiKey = process.env.VITE_RAPID_API_ARTICLE_KEY;
 console.log(rapidApiKey);
 
 export const articleApi = createApi({
